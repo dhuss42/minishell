74d55b5e63375690f80fcbc53928b2cc07ebe711
Milena Austel <maustel@2-F-3.42heilbronn.de>
2024-10-16 16:57:53 +0200

    pipe not working yet

--- exec_pipe.c
+++ exec_pipe.c
@@ -6,30 +6,53 @@
 /*   By: maustel <maustel@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 15:39:05 by maustel           #+#    #+#             */
-/*   Updated: 2024/10/16 16:14:18 by maustel          ###   ########.fr       */
+/*   Updated: 2024/10/16 16:57:34 by maustel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "executor.h"
 
-//IN CHILD:
-	// t_command	*current_cmd;
-	// 	char	*path;
-	// current_cmd = (t_command*) structi->content;
-	// if (handle_stuff(current_cmd, test))
-	// 	return (1);
-	// path = get_check_path(current_cmd->args[0], envp, test);
-	// if (!path)
-	// 	return (2);
+void	pipe_child(char *path, t_command *cmd, char **envp, t_exec *test, int *fd)
+{
+
+	dup2(fd[1], STDIN_FILENO);
+		close(fd[0]);
+		close(fd[1]);
+		// execve(path, )
+}
+
+int	first_cmd(t_list *structi, char **envp, t_exec *test, int *fd)
+{
+	pid_t	pid1;
+	char	*path;
+	t_command	*current_cmd;
+
+	current_cmd = (t_command*) structi->content;
+	if (handle_stuff(current_cmd, test))
+		return (1);
+	path = get_check_path(current_cmd->args[0], envp, test);
+	if (!path)
+		return (2);
+	pid1 = fork();
+	if (pid1 == -1)
+		return (print_error(errno, NULL, test));
+	else if (pid1 == 0)
+		pipe_child(path, current_cmd, envp, test, fd);
+	else if (pid1 > 0)
+	{
+		// parent
+	}
+	return (0);
+}
 
 int	execute_pipe(char **envp, t_list *structi, t_exec *test)
 {
-	pid_t	id;
+	pid_t	pid2;
 	int		fd[2];
 
 	if (pipe(fd) == -1)
 		return (1); //errorhandling
-	
-
+	if (first_cmd(structi, envp, test, fd))
+		return (2);
 
 }
