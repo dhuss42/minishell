5b8d37b0b4f9c7481619e57950d082bf77a94347
Milena Austel <maustel@2-F-3.42heilbronn.de>
2024-10-16 17:32:56 +0200

    think about it

--- exec_pipe.c
+++ exec_pipe.c
@@ -6,7 +6,7 @@
 /*   By: maustel <maustel@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 15:39:05 by maustel           #+#    #+#             */
-/*   Updated: 2024/10/16 16:57:34 by maustel          ###   ########.fr       */
+/*   Updated: 2024/10/16 17:31:38 by maustel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,10 @@
 
 void	pipe_child(char *path, t_command *cmd, char **envp, t_exec *test, int *fd)
 {
-
 	dup2(fd[1], STDIN_FILENO);
 		close(fd[0]);
 		close(fd[1]);
-		// execve(path, )
+		execve(path, cmd->args, envp); //maybe safe path in cmd->path??
 }
 
 int	first_cmd(t_list *structi, char **envp, t_exec *test, int *fd)
@@ -54,5 +53,7 @@ int	execute_pipe(char **envp, t_list *structi, t_exec *test)
 		return (1); //errorhandling
 	if (first_cmd(structi, envp, test, fd))
 		return (2);
+	if (second_cmd(structi, envp, test, fd))// todo
+		return (3);
 
 }
